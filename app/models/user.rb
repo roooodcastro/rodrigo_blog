1776dc3763b4efe10b226ded8b2596e27a960cb0
Rodrigo Castro Azevedo <rodrigocastro@id.uff.br>
2018-04-03 01:18:55 -0300

    Added layout to most devise views, removed summary from inside article, fixed tag styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   include Decoratable
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :trackable
+         :trackable, :validatable
 
   validates :name, presence: true
 
