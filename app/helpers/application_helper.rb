07c6f16ea168d08dfdcdc5258fcfaab362c53bc8
Rodrigo Castro Azevedo <rodrigocastro@id.uff.br>
2018-04-13 20:37:15 -0300

    Added Gravatar to comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def gravatar_url_for(email)
+    md5 = Digest::MD5.hexdigest(email.to_s)
+    "https://www.gravatar.com/avatar/#{md5}?default=identicon"
+  end
 end
