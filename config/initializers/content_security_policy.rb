8d29af69acb6d40d1589ce1ad869e00d87a203cd
Rodrigo Castro Azevedo <rodrigocastro@id.uff.br>
2018-05-11 01:28:00 -0300

    Trying to fix analytics

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,8 +3,8 @@
 Rails.application.config.content_security_policy do |p|
   p.default_src :self, :https
   p.font_src    :self, :https, :data
-  p.img_src     :self, :https, :data
+  p.img_src     :self, :https, :data, 'https://www.googletagmanager.com'
   p.object_src  :none
-  p.script_src  :self, :https
+  p.script_src  :self, :https, 'https://www.googletagmanager.com'
   p.style_src   :self, :https, :unsafe_inline
 end
